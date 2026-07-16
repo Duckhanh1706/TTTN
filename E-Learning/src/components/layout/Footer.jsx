2c730fd345acd21e61ce86d64161bbcd084fe8f3
Duckhanh1706 <nguyenduckhanh17062004@gmail.com>
2026-07-17 01:19:33 +0700

    khoi tao du an

---
+++ E-Learning/src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2026 English Learning Platform</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ E-Learning/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../../layouts/MainLayout";
+
+function Home() {
+  return (
+    <MainLayout>
+      <h1>Home Page</h1>
+    </MainLayout>
+  );
+}
+
+export default Home;
